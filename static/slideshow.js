69bc8796e9158cbee1c0532ef52999711e163b60
Adam Mansur <mansura@si.edu>
2024-09-11 13:56:58 -0400

    Updated logging, removed unused variables, and changed update trigger

--- static/slideshow.js
+++ static/slideshow.js
@@ -1,53 +1,53 @@
 (function(eq, $, undefined) {
 	
-	var baseUrl = "/ggm-seismic-display-web/static/";
-	var kind = {};
-	var images = {};
+	var baseUrl = "/ggm-seismic-display-web/static/images/";
+	var filter = {};
 	var sequence = [];
 	var queue = [];
 	var changeSlide = null;
-	var updateSlides = null;
-
 	var msPerSlide = 15000;
-	var msPerUpdate = 300000;
 	
-	eq.initPage = function(kind_) {
-		console.log("Initiating slideshow (kind=" + kind + ")")
+	eq.initPage = function(filter_) {
+		eq.log("Initiating slideshow (filter='" + filter_ + "')")
 		
-		kind = kind_;
+		filter = filter_;
 		eq.updateSlides();
 	}
 
 	eq.updateSlides = function() {
 		// Updates the slides
-		console.log("Updating slides")
-		
-		images.length = 0;
+		eq.log("Updating slides")
+
+		const d = new Date();
 		sequence.length = 0;
-		$.getJSON(baseUrl + "images.json", function(data){
+		$.getJSON(baseUrl + "images.json?t=" + d.getTime(), function(data){
 			$.each(data, function(key, val) {
 				$.each(val, function(i, v) {
-					if (v.startsWith(kind)) { sequence.push(v); }
+					if (v.startsWith(filter)) { sequence.push(v); }
 				});
 			});
-			console.log(sequence);
+			eq.log(sequence);
 			eq.setQueue();
-			// Adjust interval to land on next hour
-			updateSlides = setTimeout(function() {
-				eq.updateSlides();
-			}, eq.getMsUntil(msPerUpdate));
 		});
 	}
 
 	eq.setQueue = function() {
 		// Creates a queue of images to display
-		console.log("Updating queue")
+		eq.log("Updating queue")
+
+		// World and USA maps display more frequently
+		let world = (sequence[0].includes("-c01-")) ? sequence.shift() : null;
+		let usa = (sequence[0].includes("-c02-")) ? sequence.shift() : null;
 
 		queue.length = 0;
 		let seq = [];
 		for (var i = 0; i < 10000; i++) {
-			if (seq.length == 0) { seq = sequence.slice(0); }
-			queue.push(seq.shift());
+			if (world && i % 8 == 0) { queue.push(world); }
+			else if (usa && i % 4 == 0) { queue.push(usa); }
+			else { 
+				if (seq.length == 0) { seq = sequence.slice(0); }
+				queue.push(seq.shift());
+			}
 		}
 
 		// Align queue to start of hour
@@ -63,34 +63,45 @@
 	}
 
 	eq.changeSlide = function() {
-		console.log("Changing slide")
-
 		// Changes the slide
+		eq.log("Changing slide")
+
 		changeSlide = setTimeout(function() {
 			eq.updateImage();
 			eq.changeSlide();
 		}, eq.getMsUntil(msPerSlide));
 	}
 
-
 	eq.updateImage = function() {
-		console.log("Showing " + queue[0]);
-		
-		$("#slide").attr("src", baseUrl + queue.shift());
+		// Updates the src attribute of the image element
+		eq.log("Showing " + queue[0]);
+
+		var url = baseUrl + queue.shift();
+
+		$.ajax({
+			type: "HEAD",
+			url: url,
+			success: function() { $("#slide").attr("src", url); },
+			error: function() {
+				eq.log("Image not found: " + url);
+				eq.updateSlides();
+			}
+		})
 	}
 
 	eq.getMsUntil = function(intervalMs) {
-		// Calculates the number of milliseconds until the next run of a timeout
+		// Calculates the number of milliseconds until the next event call
 		const d = new Date()
 		let ms = d.getSeconds() * 1000 + d.getMilliseconds() + intervalMs;
 		let rem = ms % intervalMs;
-		console.log({
-			interval: intervalMs,
-			ms_exact: ms,
-			ms_remainder: rem,
-			ms_diff: intervalMs - rem
-		});
+		eq.log("Scheduled next event for " + (intervalMs - rem) + " ms");
 		return intervalMs - rem;
+	}
+	
+	eq.log = function(message) {
+		const d = new Date();
+		if (Array.isArray(message)) { message = "[" + message.join(", ") + "]"; }
+		console.log(d.toISOString() + ": " + message)
 	}
 	
 }( window.eq = window.eq || {}, jQuery ));
